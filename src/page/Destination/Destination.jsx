34b8f8b0c3f778913020dc6b2f167b822a0e0f72
Sasa <sasastanisic4@gmail.com>
2023-06-25 23:43:45 +0200

    Completed work on the destination data

--- src/page/Destination/Destination.jsx
+++ src/page/Destination/Destination.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Destination = ({ destination }) => {
+const Destination = (props) => {
   return (
     <div className="max-w-sm ml-14 rounded overflow-hidden shadow-lg">
       <img
         className="w-full h-64 object-cover"
-        src={destination.pictureUrl}
-        alt={destination.place}
+        src={props.destination.pictureUrl}
+        alt={props.destination.place}
       />
       <div className="px-6 py-4 flex justify-between items-center">
         <div>
           <h3 className="text-xl mb-2">
-            <b>{destination.place}</b>, {destination.country}
+            <b>{props.destination.place}</b>, {props.destination.country}
           </h3>
         </div>
         <div>
           <Link
-            to={`/destination/${destination.id}`}
-            className="rounded text-white font-semibold bg-green-400 hover:bg-green-600 py-2 px-6 transition duration-300 ease-in-out"
+            to={`/destination/${props.destination.id}`}
+            className="rounded text-white font-semibold bg-red-700 hover:bg-red-500 py-2 px-6 transition duration-300 ease-in-out"
           >
             Details
           </Link>
--- src/page/Destination/SingleDestination.jsx
+++ src/page/Destination/SingleDestination.jsx
@@ -1,16 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import DestinationService from "../../service/DestinationService";
 
 const SingleDestination = () => {
   const { id } = useParams();
-  //const navigate = useNavigate();
 
-  const [destination, setDestination] = useState({
-    id: id,
-    place: "",
-    country: "",
-  });
+  const [destination, setDestination] = useState({});
 
   useEffect(() => {
     const getDestinationData = async () => {
@@ -25,7 +20,37 @@ const SingleDestination = () => {
     getDestinationData();
   }, [id]);
 
-  return <div>{destination.country}</div>;
+  const { place, country, population, description, pictureUrl } = destination;
+
+  return (
+    <div className="container mx-auto my-8">
+      <div className="flex px-5 py-5">
+        <div className="w-1/2">
+          <img className="w-full" src={pictureUrl} alt={place} />
+        </div>
+        <div className="w-1/2 px-8">
+          <h2 className="text-3xl font-semibold mb-4">{place}</h2>
+          <div className="text-lg mb-4">
+            <p>
+              <b>Country:</b> {country}
+            </p>
+            <p>
+              <b>Population:</b> {population}
+            </p>
+          </div>
+          <p>{description}</p>
+          <div className="mt-8">
+            <Link
+              to={`/destination`}
+              className="mt-4 rounded text-white font-semibold bg-amber-400 hover:bg-amber-600 py-2 px-6 transition duration-300 ease-in-out"
+            >
+              Go back
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleDestination;
